b6e2ea90f06f6184c81df6d100271dc3926537e9
krup1nski <rainydn@mail.ru>
2025-05-19 23:27:47 +0300

    widgets

--- wp-content/themes/band-digital/home.php
+++ wp-content/themes/band-digital/home.php
@@ -64,69 +64,8 @@
                 </div>
 
 
+            <?php get_sidebar() ?>
 
-                <!-- BlOG SIDEBAR -->
-                <div class="col-lg-4">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <ul id="sidebar">
-                                <?php if ( ! dynamic_sidebar('sidebar-blog') ) : dynamic_sidebar('sidebar-blog')?>
-                                <?php endif; ?>
-                            </ul>
-                            <div class="sidebar-widget search">
-                                <div class="form-group">
-                                    <input type="text" placeholder="search" class="form-control">
-                                    <i class="fa fa-search"></i>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-12">
-                            <div class="sidebar-widget about-bar">
-                                <h5 class="mb-3">About us</h5>
-                                <p>Nostrum ullam porro iusto. Fugit eveniet sapiente nobis nesciunt velit cum fuga doloremque dignissimos asperiores</p>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-12">
-                            <div class="sidebar-widget category">
-                                <h5 class="mb-3">Category</h5>
-                                <ul class="list-styled">
-                                    <li>Marketing</li>
-                                    <li>Digiatl</li>
-                                    <li>SEO</li>
-                                    <li>Web Design</li>
-                                    <li>Development</li>
-                                    <li>video</li>
-                                    <li>audio</li>
-                                    <li>slider</li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-12">
-                            <div class="sidebar-widget tag">
-                                <a href="#">web</a>
-                                <a href="#">development</a>
-                                <a href="#">seo</a>
-                                <a href="#">marketing</a>
-                                <a href="#">branding</a>
-                                <a href="#">web deisgn</a>
-                                <a href="#">Tutorial</a>
-                                <a href="#">Tips</a>
-                                <a href="#">Design trend</a>
-                            </div>
-                        </div>
-                        <div class="col-lg-12">
-                            <div class="sidebar-widget download">
-                                <h5 class="mb-4">Download Files</h5>
-                                <a href="#"> <i class="fa fa-file-pdf"></i>Company Manual</a>
-                                <a href="#"> <i class="fa fa-file-pdf"></i>Company Profile</a>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
             </div>
         </div>
     </section>
